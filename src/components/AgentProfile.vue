b908c248c7f188c54b43c62c849e2b9eccd5fc0c
MarinaB2 <44431361+MarinaB2@users.noreply.github.com>
2020-08-27 15:06:32 +0200

    added props to agent component

--- src/components/AgentProfile.vue
+++ src/components/AgentProfile.vue
@@ -6,3 +6,26 @@
     * Profile Image
     * List of listings
 -->
+
+<template>
+  <div>
+    <div v-bind:key="agent.id" v-for="agent in agents"></div>
+    <h3>{{agent.first_name}} {{agent.last_name}}</h3>
+    <h4>{{agent.telephone}}</h4>
+    <h4>{{agent.email}}</h4>
+    <img src alt />
+    <!--{{agent.profile_image}}-->
+    <ul>{{agent.listings}}</ul>
+  </div>
+</template>
+
+
+<script>
+export default {
+  name: "",
+  props: ["agents"],
+};
+</script>
+
+<style scoped>
+</style>
