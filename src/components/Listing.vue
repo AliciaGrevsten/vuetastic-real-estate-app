c15be3bbdf58f4bbc4cc2b23a956eb40bf3bf620
AliciaGrevsten <alicia.grevsten@gmail.com>
2020-08-28 17:11:27 +0200

    Updated design and fixed issue with showing Listing Information

--- src/components/Listing.vue
+++ src/components/Listing.vue
@@ -1,30 +1,24 @@
-<!-- 
-* Adress
-* Real Estate Agent contact information
-* Images (full gallery)
--->
 <template>
   <div class="container">
-    <h1>Listing</h1>
-    <div class="cardgroup" v-bind:key="listing.id" v-for="listing in data">
-      <div v-bind:key="agent.id" v-for="agent in data">
-        <div class="card" v-bind:key="listing.id" v-for="listing in listing.listings">
+    <h1>More Information</h1>
+    <div class="cardgroup" v-if="isSelected">
+        <div class="card">
           <div class="card-body">
-            <div class="img" v-bind:key="image.id" v-for="image in listing.images">
-              <img v-bind:src="image" />
-            </div>
-            <p>Adress: {{ listing.address }}</p>
-            <p>Description: {{ listing.description }}</p>
-            <p>Latitude: {{ listing.latitude }}</p>
-            <p>Longitude: {{ listing.longitude }}</p>
+            <h4>Address: {{ listing.address }}</h4>
+            <h4>Description: {{ listing.description }}</h4>
+            <h4>Latitude: {{ listing.latitude }}</h4>
+            <h4>Longitude: {{ listing.longitude }}</h4>
 
             <br />
-            <p>Agent: {{agent.first_name}} {{agent.last_name}}</p>
-            <p>Telephone: {{agent.telephone}}</p>
-            <p>Email: {{agent.email}}</p>
+            <h4>Agent: {{agent.first_name}} {{agent.last_name}}</h4>
+            <h4>Phone: {{agent.telephone}}</h4>
+            <h4>Email: {{agent.email}}</h4>
+
+            <div class="img" v-bind:key="image.id" v-for="image in listing.images">
+              <img v-bind:src="image" />
+            </div> 
           </div>
         </div>
-      </div>
     </div>
   </div>
 </template>
@@ -32,22 +26,17 @@
 <script>
 export default {
   name: "Listing",
-  props: ["data"],
+  props: ["listing", "agent", "isSelected"],
 };
 </script>
 
 <style scoped>
-.container {
-  color: rgb(133, 197, 199);
-}
 .card {
-  width: 20%;
-  color: white;
-  background-color: rgb(133, 197, 199);
-  padding: 1%;
+  width: 100%;
+  color: rgb(77, 99, 119);
+  padding: 2% 4%;
+  box-sizing: border-box;
   margin: 1%;
-  float: left;
-  border-radius: 12px;
 }
 img {
   display: block;
--- src/components/Listings.vue
+++ src/components/Listings.vue
@@ -1,20 +1,29 @@
 <template>
-
   <div class="container">
+    <h1>Listings</h1>
     <div class="cardgroup" v-bind:key="agent.id" v-for="agent in displayedListings">
       <div class="card" v-bind:key="listing.address" v-for="listing in agent.listings">
-        <h4 class="card-header">{{ listing.address }}</h4>
-        <div class="card-body">
+        <div class="card-body left">
+          <img v-bind:src="listing.images[0]" alt="Picture of listing" />
+        </div>
+        <div class="card-body right">
+          <h4 class="card-header">{{ listing.address }}</h4>
           <h5 class="card-title">
             Agent:
-            <a href="#" v-bind:key="agent.id" v-on:click="$emit('show-agent', agent.id)" class="card-link">{{ agent.first_name }} {{ agent.last_name }}</a>
+            <a
+              href="#"
+              v-bind:key="agent.id"
+              v-on:click="$emit('show-agent', agent.id)"
+              class="card-link"
+            >{{ agent.first_name }} {{ agent.last_name }}</a>
           </h5>
-        </div>
-        <br />
-        <img v-bind:src="listing.images[0]" alt="Picture of listing" />
-        <br />
-        <div class="card-body">
-          <a href="#" class="card-link">Read More..</a>
+          <br />
+          <a
+            href="#"
+            v-bind:key="listing"
+            v-on:click="$emit('show-listing', listing, agent.id)"
+            class="card-link"
+          >Read More..</a>
         </div>
       </div>
     </div>
@@ -57,43 +66,50 @@ export default {
       await this.getDisplayListings();
       this.currentPage++;
     },
-  }
+  },
 };
 </script>
 
 <style scoped>
-.container {
-  width: 50%;
-  margin: auto;
-  color: rgb(77, 99, 119);
-}
 .card {
-  width: 25%;
+  width: 100%;
   color: white;
-  min-height: 350px;
+  min-height: 250px;
   align-items: stretch;
-  background-color: rgb(133, 197, 199);
+  box-sizing: border-box;
   padding: 1% 2%;
-  margin: 2%;
-  float: left;
-  border-radius: 12px;
+  margin: 1% 0% 3% 0%;
+  border-bottom: 1px dotted rgba(77, 99, 119, 0.384);
+  border-radius: 4px;
 }
 .title {
   padding: 1% 1% 4% 1%;
 }
+.left,
+.right {
+  padding: 4%;
+  float: left;
+}
+.left {
+  width: 40%;
+}
+.right {
+  width: 40%;
+}
+h5,
 h4 {
   color: rgb(77, 99, 119);
 }
 img {
-  height: 50%;
-  width: 100%;
+  height: 100%;
+  width: 90%;
   display: block;
 }
 a {
   text-decoration: none;
-  color: white;
+  color: rgb(58, 82, 104);
 }
 a:hover {
-  color: rgb(58, 82, 104);
+  color: rgb(31, 147, 255);
 }
 </style>
